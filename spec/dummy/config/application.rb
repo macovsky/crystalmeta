6149eab2e4d9be2b5dd729ef54871002ce47cee1
Weston Triemstra <weston@netsign.com>
2017-10-13 16:19:30 -0700

    Get dummy app loaded without complaint in rails 5.1

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -20,6 +20,8 @@ module Dummy
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    config.eager_load = false
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -56,9 +58,9 @@ module Dummy
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    # config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    # config.assets.version = '1.0'
   end
 end
